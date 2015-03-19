3e3c7baa2338aab9ba5a63b04e48779a678e31e2
Geri044 <geri044@hotmail.com>
2015-03-20 00:06:06 +0100

    Na itt van az amire még este juttotam. Egy kicsit jobban nézz már ki :D

--- Szoftlab4/src/Navigator.java
+++ Szoftlab4/src/Navigator.java
@@ -8,12 +8,16 @@ public class Navigator {
 	Mezo athelyez(Mezo honnan, Sebesseg sebesseg){
 		Mezo hova = null;
 		System.out.println("[navigator: Navigator]");
+		System.out.println("A honnan és a sebesseg paraméterekbõl kiszámolja, hogy melyik mezõre érkezik a robot és azzal visszatér.");
+		System.out.println("[navigator: Navigator] <--- return athelyez: hova ---");
 		return hova;
 	}
 	
 	boolean kulsoMezo(Mezo mezo){
 		boolean kulso = false;
 		System.out.println("[navigator: Navigator]");
+		System.out.println("Visszaadja, hogy egy paraméterül átadott mezõ külsõ-e.");
+		System.out.println("[navigator: Navigator] <--- return kulsoMezo: kulso ---");
 		return kulso;
 	}
 }
--- Szoftlab4/src/Sebesseg.java
+++ Szoftlab4/src/Sebesseg.java
@@ -7,17 +7,21 @@ public class Sebesseg {
 	
 	public void hozzaad(Sebesseg sebesseg){
 		System.out.println("[sebesseg: Sebesseg]");
+		System.out.println("A sebességvektor értékét megnöveli egy másik sebességvektoréval (sebesseg paraméter).");
 	}
 	
 	public void felez(Sebesseg sebesseg){
 		System.out.println("[sebesseg: Sebesseg]");
+		System.out.println("Megfelezi a paraméterül kapott sebességvektort.");
 	}
 	
 	public void modosithatatlan(){
 		System.out.println("[sebesseg: Sebesseg]");
+		System.out.println("A modosithato attribútumot beállítja hamisra.");
 	}
 	
 	public void modosithato(){
 		System.out.println("[sebesseg: Sebesseg]");
+		System.out.println("A modosithato attribútumot beállítja igazra.");
 	}
 }
