166aab1f271a3755df4c477e4aac1b02dd743f2a
riccio49 <farkas0902@gmail.com>
2015-03-21 23:21:03 +0100

    Nézétek át mert halandó vagyok! :)

--- Szoftlab4/src/Olajfolt.java
+++ Szoftlab4/src/Olajfolt.java
@@ -1,4 +1,37 @@
+public class Olajfolt implements Mezonallo{
+	
+	private Mezo mezo;
+	
+	public Olajfolt(Mezo mezo){
+		this.mezo = mezo;
+	}
+	
+	@Override
+	public void jottValaki(Mezonallo joveveny) {
+		System.out.println("[olajfolt: Olajfolt] --- olajfoltraLeptem(olajfolt) ---> [joveveny: Mezonallo]");
+		System.out.println("//Az ottlévõ olajfolt szól, hogy olajfoltra lépett a jövevény.");
+		joveveny.olajfoltraLeptem(this);
+		System.out.println("[olajfolt: Olajfolt] <--- return olajfoltraLeptem --- [joveveny: Mezonallo]");
+	}
 
-public class Olajfolt {
-	int i = 10;
+	@Override
+	public void ragacsraLeptem(Ragacs kireLeptem) {
+		System.out.println("[olajfolt: Olajfolt] --- leregisztral(ragacs) ---> [mezo: Mezo]");
+		System.out.println("// Az új olajfolt leregisztrálja a ragacsot a mezõrõl.");
+		mezo.leregisztral(kireLeptem);
+		System.out.println("[olajfolt: Olajfolt] <--- return leregisztral --- [mezo: Mezo]");
+	}
+
+	@Override
+	public void olajfoltraLeptem(Olajfolt kireLeptem) {
+		System.out.println("[olajfolt: Olajfolt] --- leregisztral(regiolajfolt) ---> [mezo: Mezo]");
+		System.out.println("// Az új olajfolt leregisztrálja a régi olajfoltot a mezõrõl.");
+		mezo.leregisztral(kireLeptem);
+		System.out.println("[olajfolt: Olajfolt] <--- return leregisztral --- [mezo: Mezo]");
+	}
+
+	@Override
+	public void robotraLeptem(Robot kireLeptem) {
+		
+	}
 }
--- Szoftlab4/src/Ragacs.java
+++ Szoftlab4/src/Ragacs.java
@@ -1,8 +1,38 @@
+public class Ragacs implements Mezonallo{
 
-public class Ragacs {
-	//ÉN egy kicsi ragacs vagyok
+	private Mezo mezo;
+	
+	public Ragacs(Mezo mezo){
+		this.mezo = mezo;
+	}
+	
+	@Override
+	public void jottValaki(Mezonallo joveveny) {
+		System.out.println("[ragacs: Ragacs] --- ragacsraLeptem(ragacs) ---> [joveveny: Mezonallo]");
+		System.out.println("//Az ott lévõ ragacs szól, hogy ragacsra lépet a jövevény.");
+		joveveny.ragacsraLeptem(this);
+		System.out.println("[ragacs: Ragacs] <--- return ragacsraLeptem --- [joveveny: Mezonallo]");
+	}
 
+	@Override
+	public void ragacsraLeptem(Ragacs kireLeptem) {
+		System.out.println("[ragacs: Ragacs] --- leregisztral(ragacs) ---> [mezo: Mezo]");
+		System.out.println("// Az új ragacs leregisztrálja a régi ragacsot a mezõrõl.");
+		mezo.leregisztral(kireLeptem);
+		System.out.println("[ragacs: Ragacs] <--- return leregisztral --- [mezo: Mezo]");
+	}
+
+	@Override
+	public void olajfoltraLeptem(Olajfolt kireLeptem) {
+		System.out.println("[ragacs: Ragacs] --- leregisztral(olajfolt) ---> [mezo: Mezo]");
+		System.out.println("//Az új ragacs leregisztrálja az olajfoltot a mezõrõl.");
+		mezo.leregisztral(kireLeptem);
+		System.out.println("[ragacs: Ragacs] <--- return leregisztral --- [mezo: Mezo]");
+	}
+
+	@Override
+	public void robotraLeptem(Robot kireLeptem) {
+		
+	}
 	
-	
-	// DE TÉNYLEG! :(
 }
