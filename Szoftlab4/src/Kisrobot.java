04188c6ac5f11ee4f59c32e8e039759b17879877
riccio49 <farkas0902@gmail.com>
2015-04-18 20:22:35 +0200

    ragacs, olaj javítandó verzió, kisrobot csak létrehozva

---
+++ Szoftlab4/src/Kisrobot.java
@@ -0,0 +1,58 @@
+
+public class Kisrobot implements Mezonallo {
+
+	@Override
+	public void jottValaki(Mezonallo joveveny) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ragacsraLeptem(Ragacs kireLeptem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void olajfoltraLeptem(Olajfolt kireLeptem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void robotraLeptem(Robot kireLeptem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void kisrobotraLeptem(Kisrobot kireLeptem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean szennyezodesVagyok() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Mezo getpozicio() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setpozicio(Mezo m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
