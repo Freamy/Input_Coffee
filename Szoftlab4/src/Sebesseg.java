0fab739defa29621f9f1809d11c5156414593a1f
sciencelunatic <sciencelunatic@gmail.com>
2015-04-18 18:08:34 +0200

    A sebességnek megírtam egy hossz függvényt ami visszaadja a hosszát. Ez a Robot megtett út számításához kell.

--- Szoftlab4/src/Sebesseg.java
+++ Szoftlab4/src/Sebesseg.java
@@ -80,11 +80,13 @@ public class Sebesseg {
 		y *= -1;
 	}
 	
+	public double hossz() {
+		return Math.sqrt(x*x+y*y);
+	}
+	
 	//Új kör kezdésekor végrehajtandó mûveletek ide kerülhetnek.
 	public void tick () {
 		//Jelenleg a sebességgel nem történik semmi új kör kezdésekor.
 		//Például ha lenne súrlódás, ami adott idõközönként csökkenti a sebességet az ide kerülhetne.
 	}
 }
-
-//Teszt komment.
